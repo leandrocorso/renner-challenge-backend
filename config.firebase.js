52c02a6d0934c225a8bc03cb759ec4d352374b0b
Leandro Corso <contato@leandrocorso.com.br>
2022-09-07 12:29:54 -0300

    API setup

---
+++ config.firebase.js
@@ -0,0 +1,16 @@
+const firebase = require('firebase')
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_APY_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const db = firebase.firestore()
+
+module.exports = db
---
+++ index.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const db = require('./config.firebase')
+
+const defaultStartMessage = `Renner Challenge backend is running on port: ${process.env.HTTP_PORT}`
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send(defaultStartMessage)
+});
+
+const products = express.Router()
+app.use('/products', products)
+
+const productsRef = db.collection('products')
+
+products.get("/", async (req, res) => {
+    try {
+        const snapshot = await productsRef.get()
+        const productsList = snapshot.docs.map(doc => ({ id:doc.id, ...doc.data() }) )
+        
+        if (productsList) {
+            res.send(productsList)
+        } else {
+            res.sendStatus(400)
+        }
+
+    } catch (error) {
+        res.sendStatus(502)
+        throw error
+    }
+});
+
+products.get("/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+        if (!id) return res.sendStatus(404)
+
+        const snapshot = await productsRef.doc(id).get()
+        const product = { id, ...snapshot.data() }
+        
+        if (product) {
+            res.status(200).send(product)
+        } else {
+            res.sendStatus(400)
+        }
+
+    } catch (error) {
+        throw error
+    }
+});
+
+app.listen(process.env.HTTP_PORT, () => console.log(defaultStartMessage))
